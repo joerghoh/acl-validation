4c80fa3936d6df1a9e9714ce7fc68c85e2dcb23e
Joerg Hoh <joerg@joerghoh.de>
2020-05-24 15:32:43 +0200

    make the exception an inner class

--- core/src/main/java/de/joerghoh/aem/aclvalidation/impl/Evaluator.java
+++ core/src/main/java/de/joerghoh/aem/aclvalidation/impl/Evaluator.java
@@ -158,5 +158,17 @@ public class Evaluator {
         
     }
     
+    public class EvaluationException extends Exception {
+        
+        EvaluationException (String message) {
+            super (message);
+        }
+        
+        EvaluationException (String message, Exception e) {
+            super (message, e);
+        }
+        
+    }
+    
     
 }
